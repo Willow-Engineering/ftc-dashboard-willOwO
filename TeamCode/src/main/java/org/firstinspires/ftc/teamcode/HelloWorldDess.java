239c1c4e4cc5552f5257daf1792a08c3488361c5
Dess <143428885+DessTheBest1@users.noreply.github.com>
2023-09-11 09:57:42 -0500

    gamepad code
    the code for the gamepad stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldDess.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class GamepadOpMode extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+        telemetry.addData("Left stick x", gamepad1.left_stick_x);
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("A button", gamepad1.a);
+        telemetry.addData("speed Forward", speedForward);
+    }
+}
