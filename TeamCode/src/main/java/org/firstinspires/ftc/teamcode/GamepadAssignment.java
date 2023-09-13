2b3d3f385c49e454dfdbd9eab8c2aa5dba6926e0
Dess <143428885+DessTheBest1@users.noreply.github.com>
2023-09-13 11:21:33 -0500

    Update
    chapter 4 assignment, telemetry for all buttons, crazy mode, turbo mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignment.java
@@ -11,21 +11,42 @@ public class GamepadAssignment extends OpMode {
 
     @Override
     public void loop() {
-        //left stick controls and buttons A+B:
+        //speed variables
         double speedForward = -gamepad1.left_stick_y / 2.0;
+        double sideSpeed = -gamepad1.right_stick_y / 2.0;
+        //turbo mode
+        if(!gamepad1.a) {
+            speedForward *= 0.5;
+        }
+        //left stick controls
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        //A-B-X-Y buttons
         telemetry.addData("A button", gamepad1.a);
         telemetry.addData("B button", gamepad1.b);
+        telemetry.addData("X button", gamepad1.x);
+        telemetry.addData("Y button", gamepad1.y);
+        //Trigger Buttons
         telemetry.addData("Right trigger", gamepad1.right_trigger);
         telemetry.addData("Left trigger", gamepad1.left_trigger);
+        float triggerSum = gamepad1.left_trigger + gamepad1.right_trigger;
+        telemetry.addData("Trigger sum", triggerSum);
+        //speedForward reporting
         telemetry.addData("speed Forward", speedForward);
-        //right stick controls:
+        //right stick controls and variable
+        telemetry.addData("speed Sideways", sideSpeed);
         telemetry.addData("Right stick x", gamepad1.right_stick_x);
         telemetry.addData("Right stick y", gamepad1.right_stick_y);
-        telemetry.addData("Trigger sum", gamepad1.right_trigger + gamepad1.left_trigger);
         if (gamepad1.left_stick_y < -0.5) {
             telemetry.addData("Left stick", "is negative and large");
+        }
+        if (gamepad1.right_stick_y < -0.5) {
+            telemetry.addData("Right stick", "is negative and large");
+        }
+        //crazy mode code
+        if (gamepad1.a) {
+            speedForward = gamepad1.left_stick_x;
+            sideSpeed = gamepad1.left_stick_y;
         }
     }
 }
