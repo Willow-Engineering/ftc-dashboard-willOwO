7ddc7a43f41d57b62f4436bc5c2a41c5531ef5ad
Dess <143428885+DessTheBest1@users.noreply.github.com>
2023-09-18 10:00:08 -0500

    Update GamepadAssignment.java
    my work from last class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignment.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -10,23 +11,31 @@ public class GamepadAssignment extends OpMode {
     }
 
     @Override
-    public void loop() {
+    public void loop() {0
+
         //speed variables
         double speedForward = -gamepad1.left_stick_y / 2.0;
         double sideSpeed = -gamepad1.right_stick_y / 2.0;
-        //turbo mode
-        if(!gamepad1.a) {
-            speedForward *= 0.5;
-        }
-        //left stick controls
+        boolean aButton;
+        boolean bButton;
+        boolean xButton;
+        boolean yButton;
+
+        //left stick controls and variables
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        if (gamepad1.left_stick_y < -0.5) {
+            telemetry.addData("Left stick", "is negative and large");
+        }
+        if (gamepad1.left_stick_y > 0.5) {
+            telemetry.addData("Right stick", "is positive and large");
+        }
         //A-B-X-Y buttons
         telemetry.addData("A button", gamepad1.a);
         telemetry.addData("B button", gamepad1.b);
         telemetry.addData("X button", gamepad1.x);
         telemetry.addData("Y button", gamepad1.y);
-        //Trigger Buttons
+        //trigger buttons and trigger sum
         telemetry.addData("Right trigger", gamepad1.right_trigger);
         telemetry.addData("Left trigger", gamepad1.left_trigger);
         float triggerSum = gamepad1.left_trigger + gamepad1.right_trigger;
@@ -37,11 +46,15 @@ public class GamepadAssignment extends OpMode {
         telemetry.addData("speed Sideways", sideSpeed);
         telemetry.addData("Right stick x", gamepad1.right_stick_x);
         telemetry.addData("Right stick y", gamepad1.right_stick_y);
-        if (gamepad1.left_stick_y < -0.5) {
-            telemetry.addData("Left stick", "is negative and large");
-        }
         if (gamepad1.right_stick_y < -0.5) {
             telemetry.addData("Right stick", "is negative and large");
+        }
+        if (gamepad1.right_stick_y > 0.5) {
+            telemetry.addData("Right stick", "is positive and large");
+        }
+        //turbo mode
+        if(!gamepad1.a) {
+            speedForward *= 0.5;
         }
         //crazy mode code
         if (gamepad1.a) {
