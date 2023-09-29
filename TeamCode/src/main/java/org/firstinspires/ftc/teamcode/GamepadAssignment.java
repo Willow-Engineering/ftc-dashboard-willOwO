11bbc76cf120fd890b3d2d274ddcf4b0bc9fa6cb
Dess <143428885+DessTheBest1@users.noreply.github.com>
2023-09-29 10:19:34 -0500

    eeee
    eeeeeeeeee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadAssignment.java
@@ -1,28 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp()
 public class GamepadAssignment extends OpMode {
     private DcMotor motor1;
+    private Servo servo1;
     double speedForward = -gamepad1.left_stick_y / 2.0;
     double sideSpeed = -gamepad1.right_stick_y / 2.0;
     boolean aButton;
     boolean bButton;
     boolean xButton;
     boolean yButton;
+    double triggerSum;
 
     @Override
     public void init() {
-        motor1 = hardwareMap.get(DcMotor.class, "testMotor1");
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        servo1 = hardwareMap.get(Servo.class, "servo1");
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
     }
 
     @Override
     public void loop() {
+        triggerSum = gamepad1.left_trigger + gamepad1.right_trigger;
 
         //left stick controls and variables
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
@@ -41,7 +47,6 @@ public class GamepadAssignment extends OpMode {
         //trigger buttons and trigger sum
         telemetry.addData("Right trigger", gamepad1.right_trigger);
         telemetry.addData("Left trigger", gamepad1.left_trigger);
-        float triggerSum = gamepad1.left_trigger + gamepad1.right_trigger;
         telemetry.addData("Trigger sum", triggerSum);
         //speedForward reporting
         telemetry.addData("speed Forward", speedForward);
@@ -67,6 +72,9 @@ public class GamepadAssignment extends OpMode {
         //motor control code, runs motor 1 when pressing B
         if (gamepad1.b) {
             motor1.setPower(1);
+        }
+        if (gamepad1.y) {
+            servo1.setPosition(80);
         }
     }
 }
