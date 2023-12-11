2abfac026e49f5f355aea6eaf35ead0d89809b06
Dess <143428885+DessTheBest1@users.noreply.github.com>
2023-12-11 10:24:59 -0600

    Update AprilTag.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag.java
@@ -52,7 +52,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: AprilTag Easy", group = "Concept")
-@Disabled
+//@Disabled
 public class AprilTag extends LinearOpMode {
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
